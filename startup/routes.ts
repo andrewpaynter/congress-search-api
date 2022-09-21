73a680695b1dfefe69589473c9cbc2113d3dff85
Andrew Paynter <andrewpaynter97@gmail.com>
2022-09-21 11:41:31 -0400

    updated cors info

--- startup/routes.ts
+++ startup/routes.ts
@@ -7,7 +7,7 @@ const img = require( '../routes/img')
 
 module.exports = function (app: ExpressApp) {
   app.use(express.json())
-  app.use(cors({ origin: ['http://localhost:3000'], credentials: true, exposedHeaders: 'finalItem,Content-Type'}))
+  app.use(cors({ origin: ['http://www.congress-searcher.herokuapp.com/'], credentials: true, exposedHeaders: 'finalItem,Content-Type'}))
 
   app.use('/api/congress', congress)
   app.use('/api/img', img)
