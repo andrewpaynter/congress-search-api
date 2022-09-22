80260196225f9f942dfe1e1c79941df5cfcd7908
Andrew Paynter <andrewpaynter97@gmail.com>
2022-09-22 09:51:47 -0400

    removed origin requirement with cors

--- startup/routes.ts
+++ startup/routes.ts
@@ -8,7 +8,7 @@ const img = require( '../routes/img')
 
 module.exports = function (app: ExpressApp) {
   app.use(express.json())
-  app.use(cors({ credentials: true, exposedHeaders: 'finalItem,Content-Type'}))
+  app.use(cors({ origin: '*', credentials: true, exposedHeaders: 'finalItem,Content-Type'}))
 
   app.use('/api/congress', congress)
   app.use('/api/img', img)
