63afd9ae602d622df0de7746cf6e6aa406c1e142
Andrew Paynter <andrewpaynter97@gmail.com>
2022-09-22 09:49:22 -0400

    removed origin requirement wiht cors

--- startup/routes.ts
+++ startup/routes.ts
@@ -1,13 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import ExpressApp from '../models/ExpressApp'
+require('custom-env').env('production')
 const congress = require( '../routes/congress')
 const img = require( '../routes/img')
 
 
 module.exports = function (app: ExpressApp) {
   app.use(express.json())
-  app.use(cors({ origin: ['http://congress-searcher.herokuapp.com/'], credentials: true, exposedHeaders: 'finalItem,Content-Type'}))
+  app.use(cors({ credentials: true, exposedHeaders: 'finalItem,Content-Type'}))
 
   app.use('/api/congress', congress)
   app.use('/api/img', img)
